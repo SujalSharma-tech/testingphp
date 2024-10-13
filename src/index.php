e89af7043f29fe6965214df9a06e1c7881508e44
Sujal <112748674+SujalSharma-tech@users.noreply.github.com>
2024-10-13 11:12:26 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -37,22 +37,45 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
     $token = generateToken($email, $password);
     setcookie("token", $token, $expirationTime, "/", "localhost", true, true);
+    // $db_name = "mysql:hostname=localhost;dbname=testing";
+    // $username = "root";
+    // $password = "";
+    $db_name = "mysql:hostname=baoujjgsjzuqlvv6pv3a-mysql.services.clever-cloud.com;dbname=baoujjgsjzuqlvv6pv3a";
+    $username = "ukmwxvkswbgi855r";
+    $password = "E8AEDEOIVkTIQ8jhuJxf";
 
-    $db_name = "mysql:hostname=mysql_db;dbname=example";
-    $username = "example";
-    $password = "example";
+    // $conn = new PDO($db_name, $username, $password);
+    // $conn2 =
+    $mysqli = new mysqli("baoujjgsjzuqlvv6pv3a-mysql.services.clever-cloud.com", "ukmwxvkswbgi855r", "E8AEDEOIVkTIQ8jhuJxf", "baoujjgsjzuqlvv6pv3a");
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+        exit();
+    } else {
+        echo "Connected";
+    }
 
-    $conn = new PDO($db_name, $username, $password);
+    // $sql0 = $mysqli->query("INSERT INTO Students (Id, Name, Age, City) VALUES (2,'Aman',20,'Phg')");
+    $first_name = "Sujal";
+    $last_name = "Sharma";
+    $email = "sharmasujal995@example.com";
+    $password = "hashed_password";
+    $phone_number = "8474726477";
+    $is_verified = 1;
+    $is_landlord = 0;
+    $stmt = $mysqli->prepare("INSERT INTO users (first_name, last_name, email, password, phone_number, is_verified, is_landlord) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $first_name, $last_name, $email, $password, $phone_number, $is_verified, $is_landlord);
+    $stmt->execute();
 
-    $sql1 = $conn->query("SELECT * FROM Students");
-    $sql2 = $conn->query("SELECT * FROM Students");
+    $sql1 = $mysqli->query("SELECT * FROM users");
 
-    while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+    // print_r($sql1->fetch_all(MYSQLI_ASSOC));
+
+
+    while ($row = $sql1->fetch_all(MYSQLI_ASSOC)) {
         echo "<pre>";
         print_r($row);
         echo "</pre>";
     }
-
 
     echo json_encode([
         'token' => $token,
